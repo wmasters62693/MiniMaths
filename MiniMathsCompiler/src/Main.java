009a07562d5345f6f14de6e9e6a8a9a306ffdcfd
William Masters <wmasters.62693@farnborough.ac.uk>
2017-10-05 10:27:36 +0100

    Add files via upload

---
+++ MiniMathsCompiler/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class Main {
+    public static void main (String[] args) {
+        getFile();
+
+    }
+
+    public static void getFile() {
+        File fileL;
+        return(fileL);
+    }
+}
